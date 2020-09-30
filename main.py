75594a0d2f919853ad1dddf51bd452dfafc71197
Aidan Roth <aidanroth@gmail.com>
2020-09-30 12:04:32 -0700

    Finished implementing intToString function
    This function converts integers like 1136 into strings like dcbad.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import math
 
-
 def strToInt(string):
     intSum = 0
     for i in range(len(string)):
         if string[i] == 'a':
-           intSum += pow(4, i)
+            intSum += pow(4, i)
         elif string[i] == 'b':
             intSum += 2 * pow(4, i)
         elif string[i] == 'c':
@@ -16,16 +15,35 @@ def strToInt(string):
             intSum += 0
     return intSum
 
+def intToStr(int):
 
+    newStr = ""
+    while int > 0:
+        remainder = int % 4
+        if remainder == 1 or remainder == 2 or remainder == 3:
+            if remainder == 1:
+                newStr += "a"
+            elif remainder == 2:
+                newStr += "b"
+            elif remainder == 3:
+                newStr += "c"
+            else:
+                print("Error: invalid remainder value. Exiting")
+                quit(1)
+            int = (int - remainder) / 4
 
+        elif remainder == 0:
+            newStr += "d"
+            int = (int - 4) / 4
+        else:
+            print("Error: invalid remainder value. Exiting")
+            quit(2)
 
+    return newStr
 
 def main():
-    print(strToInt('a'))
-    print(strToInt("bd"))
+
     print(strToInt("dcbad"))
-
-
-
+    print(intToStr(1136))
 
 main()
